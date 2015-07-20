7b82ed1f74ce3b949ed621c7a7d1f83511238fec
Administrator <Administrator@PC-201507151314>
2015-07-20 10:05:57 +0800

    第一次提交，做好了字母排序列表

---
+++ 排序和分组列子/src/org/initialltouchlist/GroupLayout.java
@@ -0,0 +1,16 @@
+package org.initialltouchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.listsildedel.R;
+
+public class GroupLayout extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_group);
+	}
+
+}
